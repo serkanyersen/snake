cd395027523a652fba48f78354dcd4d313e10b93
Serkan Yersen <serkanyersen@gmail.com>
2019-06-05 11:22:26 -0700

    little upgrade

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,27 +1,27 @@
-import {SIZE} from "./constants";
+import { SIZE } from './constants';
 
 namespace Utils {
-    export function rand(min: number, max: number, reduce: number = SIZE): number {
-        let num = Math.floor(Math.random() * (max - min)) + min;
-        return num - (num % reduce);
-    }
+  export const rand = (min: number, max: number, reduce: number = SIZE): number => {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num - (num % reduce);
+  };
 
-    export function snap(num: number, point = SIZE): number {
-        let bottom = num - num % point;
-        let top = bottom + point;
+  export const snap = (num: number, point = SIZE): number => {
+    const bottom = num - (num % point);
+    const top = bottom + point;
 
-        return num - bottom <= top - num ? bottom : top;
-    }
+    return num - bottom <= top - num ? bottom : top;
+  };
 
-    export function removeNode(el: Element): void {
-        if (el.parentNode) {
-            el.parentNode.removeChild(el);
-        }
+  export const removeNode = (el: Element): void => {
+    if (el.parentNode) {
+      el.parentNode.removeChild(el);
     }
+  };
 
-    export function bound(num: number, min: number, max: number): number {
-        return Math.max(Math.min(num, max), min);
-    }
+  export const bound = (num: number, min: number, max: number): number => {
+    return Math.max(Math.min(num, max), min);
+  };
 }
 
 export default Utils;
